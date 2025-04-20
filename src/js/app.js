ee24ada071228d0e2613dc87242163aa1dbe76fc
Alex Hedley <1573469+AlexHedley@users.noreply.github.com>
2025-04-20 10:43:16 +0100

    Add pivot for winners

--- src/js/app.js
+++ src/js/app.js
@@ -13,34 +13,33 @@ myApp.controller('myController', function ($scope, $http, $q, $filter) {
         $http.get(file)
         .then(function(response) {
             $scope.games = response.data.games;
-            // $scope.generatePivot();
+            $scope.generatePivot();
         });
     };
 
-    // $scope.generatePivot = () => {
-        
-    //     $scope.data = $scope.games.map(game => game.players);
-    //     var data = [].concat.apply([], $scope.data);
+    $scope.generatePivot = () => {
+        $scope.data = $scope.games.map(game => game.players.filter(player => player.position === 1));
+        var data = [].concat.apply([], $scope.data);
 
-    //     if ($scope.ui) {
-    //         $("#output").pivotUI(
-    //             data,
-    //             {
-    //                 rows: ["name"],
-    //                 cols: ["position"]
-    //             }
-    //         );
-    //     } else {
-    //         $("#output").pivot(
-    //             data,
-    //             {
-    //                 rows: ["name"],
-    //                 cols: ["position"]
-    //             }
-    //         );
-    //     }
+        if ($scope.ui) {
+            $("#output").pivotUI(
+                data,
+                {
+                    rows: ["name"],
+                    cols: ["position"]
+                }
+            );
+        } else {
+            $("#output").pivot(
+                data,
+                {
+                    rows: ["name"],
+                    cols: ["position"]
+                }
+            );
+        }
 
-    // }
+    }
 
     $scope.init();
 });
